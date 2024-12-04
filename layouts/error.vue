25d945219edb94ea597a881b0b3d3bd726ed4dd0
fateme ebrahimzadeh <f.ebrahimzadeh@alibaba.ir>
2024-12-04 06:06:08 +0330

    feat(core): change error layout

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,18 +1,32 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
+    <v-container class="text-center fill-height">
+      <v-row justify="center" align="center">
+        <v-col cols="12" md="6">
+          <v-card class="pa-4" color="grey darken-3">
+            <v-card-title class="headline white--text">
+              <v-icon large left>{{ error.statusCode === 404 ? 'mdi-alert-circle-outline' : 'mdi-alert-outline' }}</v-icon>
+              {{ error.statusCode === 404 ? pageNotFound : otherError }}
+            </v-card-title>
+            <v-card-text>
+              <p v-if="error.statusCode === 404">The page you are looking for doesn't exist.</p>
+              <p v-else>Sorry, something went wrong on our end. Please try again later.</p>
+            </v-card-text>
+            <v-card-actions>
+              <NuxtLink to="/">
+                <v-btn color="primary">Go Back to Home</v-btn>
+              </NuxtLink>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'EmptyLayout',
+  name: 'ErrorLayout',
   layout: 'empty',
   props: {
     error: {
@@ -23,21 +37,28 @@ export default {
   data() {
     return {
       pageNotFound: '404 Not Found',
-      otherError: 'An error occurred',
-    }
+      otherError: 'An unexpected error occurred',
+    };
   },
   head() {
     const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+      this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
     return {
       title,
-    }
+    };
   },
-}
+};
 </script>
 
 <style scoped>
-h1 {
-  font-size: 20px;
+.fill-height {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.white--text {
+  color: white !important;
 }
 </style>
